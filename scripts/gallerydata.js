8b0107cde65ba3b50f650df7979d272e68d6e7fe
dnbriseno <dnbriseno@amarillocollege.com>
2026-04-19 15:04:46 -0500

    Add files via upload

---
+++ scripts/gallerydata.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+// Title of the slideshow
+let lightboxTitle = "Image Gallery";
+
+// Names of the image files shown in the slideshow
+let imgFiles = ["images/bed1.jpg","images/lookoutsuite1.jpg","images/lookoutsuite2.jpg"]
+
+// Captions associated with each image
+let imgCaptions = new Array(3);
+imgCaptions[0]="Hotel Room 1";
+imgCaptions[1]="Hotel Room 2";
+imgCaptions[2]="Hotel Room 3";
+
+
+// Count of images in the slideshow
+let imgCount = imgFiles.length;
